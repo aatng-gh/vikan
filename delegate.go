23b3ed15df256ee385e57e762ba0cea78c70256b
Aaron Nguyen <37777232+aatng-gh@users.noreply.github.com>
2025-05-02 18:04:23 -0400

    refactor: derive NoCursorDelegate from BaseDelegate

--- delegate.go
+++ delegate.go
@@ -5,9 +5,7 @@ import "github.com/charmbracelet/bubbles/list"
 var (
 	BaseDelegate     = list.NewDefaultDelegate()
 	NoCursorDelegate = func() list.DefaultDelegate {
-		d := list.NewDefaultDelegate()
-
-		// make "selected" look just like "normal"
+		d := BaseDelegate
 		d.Styles.SelectedTitle = d.Styles.NormalTitle
 		d.Styles.SelectedDesc = d.Styles.NormalDesc
 		return d
